e576a6a49f0c21cf43c9dcbe145469d4e865bf54
bfrancom <5726729+bfrancom@users.noreply.github.com>
2023-09-06 13:58:40 -0600

    adding pip install doh

--- github_reporting_tool.py
+++ github_reporting_tool.py
@@ -1,53 +1,54 @@
-#Written by Ben Francom (benfran.com)
-#for EISMGard LLC (eismgard.com) under the MIT License
+# Written by Ben Francom (benfran.com)
+# for EISMGard LLC (eismgard.com) under the MIT License
+"""Module providingFunction os environment access"""
 import os
-#from unicodedata import name
+
+# from unicodedata import name
 from github import Github
 
 # using an access token
-g = Github(os.environ['GITHUB_TOKEN'])
-org = g.get_organization(os.environ['GITHUB_ORG_NAME'])
+g = Github(os.environ["GITHUB_TOKEN"])
+org = g.get_organization(os.environ["GITHUB_ORG_NAME"])
 
-#env chiggity check
-if os.environ.get('GITHUB_TOKEN') is None:
-    print('!!! missing GITHUB_TOKEN environment variable !!!')
-if os.environ.get('GITHUB_ORG_NAME') is None:
-    print('!!! missing GITHUB_ORG_NAME environment variable !!!')
+# env chiggity check
+if os.environ.get("GITHUB_TOKEN") is None:
+    print("!!! missing GITHUB_TOKEN environment variable !!!")
+if os.environ.get("GITHUB_ORG_NAME") is None:
+    print("!!! missing GITHUB_ORG_NAME environment variable !!!")
 
 
-#Get list of repos
+# Get list of repos
 print("Repo List:")
 repos = org.get_repos()
 for r in repos:
-    print("  ",r.git_url)
-    
+    print("  ", r.git_url)
 
-#Get list of teams
+# Get list of teams
 print("\nTeam List:")
 teams = org.get_teams()
 for t in teams:
-    print("   ",t.name)
+    print("   ", t.name)
     team_repos = t.get_repos()
     for repos in team_repos:
-        print("     ",repos.git_url)
+        print("     ", repos.git_url)
 
-#Get list of team members
+# Get list of team members
 print("\nTeam Membership List:")
 teams = org.get_teams()
 for t in teams:
-    print("  ",t.name," Team Members:")
+    print("  ", t.name, " Team Members:")
     for m in t.get_members():
-        #print("      Name: ",m.name,", Email: ",m.email,", ID: ",m.id,", \
+        # print("      Name: ",m.name,", Email: ",m.email,", ID: ",m.id,", \
         # Login: ",m.login)
-        print("      ",m.login)
+        print("      ", m.login)
 
-#Get list of repos
+# Get list of repos
 print("\nDirect Repo Rights:")
 repos = org.get_repos()
 for r in repos:
-    print("  ",r.git_url)
+    print("  ", r.git_url)
     collaborators = r.get_collaborators()
     for c in collaborators:
-        #print("      Name: ",c.name,", Email: ",c.email,", ID: ",c.id,", Login: ", \
+        # print("      Name: ",c.name,", Email: ",c.email,", ID: ",c.id,", Login: ", \
         # c.login)
-        print("      ",c.login)
+        print("      ", c.login)
